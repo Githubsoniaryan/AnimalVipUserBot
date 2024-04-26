1eb9451da7649e9f43be00fde4f78cb1b2625b47
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2024-04-26 11:07:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,16 +23,16 @@
 from os import getenv
 from STORMDB.data import STORMS
 
-API_ID = int(getenv("API_ID", "25981592"))
-API_HASH = getenv("API_HASH", "709f3c9d34d83873d3c7e76cdd75b866")
+API_ID = int(getenv("API_ID", "25574934"))
+API_HASH = getenv("API_HASH", "029944bda8b241aacbb38b7eaa58d851")
 SESSION1 = getenv("SESSION")
-ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/5d4a2dbf4f196fcdfe4d2.mp4")
-HELP_PIC = getenv("HELP_PIC", "https://graph.org/file/e0fcedd2df8ac254bb506.jpg")
-OWNER_ID = int(getenv("OWNER_ID", "6257927828"))
+ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/ec19cf227791a167abedc.jpg")
+HELP_PIC = getenv("HELP_PIC", "https://te.legra.ph/file/ec19cf227791a167abedc.jpg")
+OWNER_ID = int(getenv("OWNER_ID", "1777910193"))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 OPENAIKEY = getenv("OPENAIKEY")
-PM_PIC = "https://graph.org/file/bf1fdd404a82d508a7ed5.jpg"
+PM_PIC = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
 SESSION2 = getenv("SESSION2")
 SESSION3 = getenv("SESSION3")
 SESSION4 = getenv("SESSION4")
